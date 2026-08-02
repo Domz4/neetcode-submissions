b788ced891a00b11c2d8487152a45ae5b130f989
Domze <108986452+Domz4@users.noreply.github.com>
2026-08-02 21:22:19 +0200

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int[] maxf = new int[26];
+
+        // so this becomes table of frequencies for a string AAABBA is 
+        // [4, 2, 0, 0 ...];
+
+        int left = 0;
+        int max = k;
+        int maxFrequency = 0;
+
+        for(int right=0; right<s.length(); right++) {
+            int charIndex = s.charAt(right) - 'A';
+            maxf[charIndex]++;
+
+            maxFrequency = Math.max(maxFrequency, maxf[charIndex]);
+            
+            int validity = (right - left + 1) - maxFrequency;
+            
+            if (validity > k) {
+                // invalid shring window
+                // remove character from the maxf table
+                maxf[s.charAt(left) - 'A']--; 
+                left++;
+
+            }
+            
+            max = Math.max(max, right - left + 1);
+        }
+        
+        return max;
+    }
+}
